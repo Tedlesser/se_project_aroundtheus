b2d224a3c3f4194d718e6d2f20d7e9b896bc18e4
Theo101 <ted.a.lesser@gmail.com>
2024-03-14 15:49:55 -0400

    Update 9 2nd Revision

---
+++ src/components/Api.js
@@ -0,0 +1,15 @@
+getInitialCards() {
+    return fetch("https://around-api.en.tripleten-services.com/v1", {
+      headers: {
+        authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6"
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        // if the server returns an error, reject the promise
+        return Promise.reject(`Error: ${res.status}`);
+      });
+  }
+
